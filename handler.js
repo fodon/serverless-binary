ab0dadda8c4ac823178df429ce1e140fab235193
Ajo Fod <ajo.fod@gmail.com>
2019-01-23 16:24:02 -0800

    Changed output so its not a json object.

--- handler.js
+++ handler.js
@@ -5,11 +5,10 @@ var fs = require('fs');
 module.exports.hello = (event, context, callback) => {
     fs.readFile("templates/i-130.pdf", function (err, data) {
         callback(null, {
-            statusCode: 200,
-            headers: {'Content-Type': 'application/pdf'},
-            body: JSON.stringify({
-                message: data.toString('base64')
+                statusCode: 200,
+                headers: {'Content-Type': 'application/pdf'},
+                body: data.toString('base64'),
+                isBase64Encoded: true
             })
         });
-    });
-};
+    };
